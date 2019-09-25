d4a9d8a595990390cdc3266b32bdead4fd27769f
Zertuk <zertukis@gmail.com>
2019-09-25 16:43:49 -0700

    reverthttps change

--- color-cart-server/server.js
+++ color-cart-server/server.js
@@ -1,13 +1,8 @@
 let express = require('express');
 let app = express();
 let port = process.env.PORT || 3001;
-let https = require('https');
-let fs = require('fs');
 
-https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-}, app).listen(port);
+app.listen(port);
 
 console.log('server started on: ' + port);
 
